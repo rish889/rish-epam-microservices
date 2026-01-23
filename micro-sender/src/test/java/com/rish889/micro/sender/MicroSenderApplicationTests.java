0ca181eeff972c4f67acb6315f7ea27e8cfe5ba2
Rishabh Garcha <rish889@gmail.com>
2026-01-23 15:11:52 +0530

    rishCommit

---
+++ micro-sender/src/test/java/com/rish889/micro/sender/MicroSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rish889.micro.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
